cc6839aa904e444736d2c2ea135c4955016966b1
EvanYap <evanyap.14@gmail.com>
2016-10-12 10:21:35 -0700

    Make first class for this git repo

--- src/ArrayHelper.java
+++
@@ -1,9 +0,0 @@
-/****
- * A helper class for 2D arrays with useful helpful methods.
- * 
- * @author EvanYap
- *
- */
-public class ArrayHelper {
-
-}
---
+++ src/CSVData.java
@@ -0,0 +1,9 @@
+/****
+ * A class to read/write numerical CSV files and allow easy access of data.
+ * 
+ * @author EvanYap
+ *
+ */
+public class CSVData {
+
+}
